59bfa5a0088b2ca0e30b511600ab66eb5797c184
wjchao9111 <wjchao9111@sina.com>
2016-05-24 22:51:37 +0800

    修改wxlt_service 使用纯django的方式实现

--- api/wxlt_service.py
+++ api/wxlt_service.py
@@ -2,6 +2,7 @@
 import xml.etree.ElementTree as ET
 from base64 import b64encode, b64decode
 
+from django.shortcuts import render
 from pyDes import triple_des, CBC, PAD_PKCS5
 from spyne.application import Application
 from spyne.decorator import rpc
@@ -130,112 +131,160 @@ class WxltTools:
         return rsp_xml
 
 
+def echo(echoRequest):
+    print echoRequest
+    echoResponse = echoRequest
+    return echoResponse
+
+
+def smsSend(smsSendRequest):
+    log(
+        title='WXLT_SERVICE_SMS_SEND_REQUEST:%s' % (smsSendRequest),
+        content=None,
+        logger='spyne.application',
+        level='debug'
+    )
+    pinid, user_id, Mobiles, Content = WxltTools.parse_send_req_xml(smsSendRequest)
+    smsSendResponse = ''
+    if user_id:
+        if Content and Mobiles:
+            uid = user_id
+            j_task = {
+                'type': 'default',
+                'name': 'API',
+                'priority': 3,
+                'content': Content,
+                'phones': Mobiles,
+            }
+            task = get_task(j_task)
+            task.user_id = uid
+            task.save()
+            taskcount, id_list = send_task_prepare_sync(task)
+
+            mobile_list = Mobiles.split(',')
+            smsSendResponse = WxltTools.gen_send_rsp_xml(pinid, '0000',
+                                                         [{'id': id_list[i], 'mobile': mobile_list[i]} for i in
+                                                          range(min(len(id_list), len(mobile_list)))])
+        else:
+            smsSendResponse = WxltTools.gen_send_rsp_xml(pinid, '0001', [])
+    else:
+        smsSendResponse = WxltTools.gen_send_rsp_xml(pinid, '0002', [])
+
+    log(
+        title='WXLT_SERVICE_SMS_SEND_RESOONSE:%s' % (smsSendResponse),
+        content=None,
+        logger='spyne.application',
+        level='debug'
+    )
+    return smsSendResponse
+
+
+def smsGetReport(smsGetReportRequest):
+    log(
+        title='WXLT_SERVICE_SMS_GET_REPORT_REQUEST:%s' % (smsGetReportRequest),
+        content=None,
+        logger='spyne.application',
+        level='debug'
+    )
+    root, pinid, user_id = WxltTools.parse_req_xml_head(smsGetReportRequest)
+    smsGetReportResponse = ''
+    if user_id:
+        uid = user_id
+        sms_list = []
+        for i in range(100):
+            msg = redis_pop_obj('status_report', uid)
+            if msg is None:
+                break
+            else:
+                sms_list.append(msg)
+        smsGetReportResponse = WxltTools.gen_rpt_rsp_xml(pinid, '0000', sms_list)
+    else:
+        smsGetReportResponse = WxltTools.gen_send_rsp_xml(pinid, '0002', [])
+    log(
+        title='WXLT_SERVICE_SMS_GET_REPORT_RESPONSE:%s' % (smsGetReportResponse),
+        content=None,
+        logger='spyne.application',
+        level='debug'
+    )
+    return smsGetReportResponse
+
+
+def smsReceive(smsReceiveRequest):
+    log(
+        title='WXLT_SERVICE_SMS_RECEIVE_REQUEST:%s' % (smsReceiveRequest),
+        content=None,
+        logger='spyne.application',
+        level='debug'
+    )
+    root, pinid, user_id = WxltTools.parse_req_xml_head(smsReceiveRequest)
+    smsReceiveResponse = ''
+    if user_id:
+        uid = user_id
+        sms_list = []
+        for i in range(100):
+            msg = redis_pop_obj('recv_sms', uid)
+            if msg is None:
+                break
+            else:
+                sms_list.append(msg)
+        smsReceiveResponse = WxltTools.gen_recv_rsp_xml(pinid, '0000', sms_list)
+    else:
+        smsReceiveResponse = WxltTools.gen_recv_rsp_xml(pinid, '0002', [])
+    log(
+        title='WXLT_SERVICE_SMS_RECEIVE_RESPONSE:%s' % (smsReceiveResponse),
+        content=None,
+        logger='spyne.application',
+        level='debug'
+    )
+    return smsReceiveResponse
+
+
+def service(request):
+    if request.method == 'GET':
+        host = request.META['HTTP_HOST']
+        return render(request, 'wxlt_wsdl.xml', {'host': host}, content_type='application/xml')
+    elif request.method == 'POST':
+        soapenv = '{http://schemas.xmlsoap.org/soap/envelope/}'
+        tns = '{wxlt.api.mas.sjz.cmcc.com}'
+        action = request.META.get('HTTP_SOAPACTION')
+        if action: action = action[1:-1]
+
+        root = ET.fromstring(request.body)
+        action_request = root.find(soapenv + 'Body' + '/' + tns + action + '/' + tns + action + 'Request')
+
+        if root.tag == soapenv + 'Envelope' and action != None and action_request != None:
+            action_response = {
+                'echo': echo,
+                'smsSend': smsSend,
+                'smsGetReport': smsGetReport,
+                'smsReceive': smsReceive
+            }[action](action_request.text)
+            return render(request, 'wxlt_resp.xml', {'action': action, 'response': action_response},
+                          content_type='application/xml')
+
+        else:
+            resp = render(request, 'wxlt_500.xml', {}, content_type='application/xml', status=500)
+
+
 class WxltService(ServiceBase):
     @rpc(Unicode, _returns=Unicode)
-    def echo(self, name):
-        print name
-        return name
+    def echo(self, echoRequest):
+        echoResponse = echo(echoRequest)
+        return echoResponse
 
     @rpc(Unicode, _returns=Unicode)
     def smsSend(self, smsSendRequest):
-        log(
-            title='WXLT_SERVICE_SMS_SEND_REQUEST:%s' % (smsSendRequest),
-            content=None,
-            logger='spyne.application',
-            level='debug'
-        )
-        pinid, user_id, Mobiles, Content = WxltTools.parse_send_req_xml(smsSendRequest)
-        smsSendResponse = ''
-        if user_id:
-            if Content and Mobiles:
-                uid = user_id
-                j_task = {
-                    'type': 'default',
-                    'name': 'API',
-                    'priority': 3,
-                    'content': Content,
-                    'phones': Mobiles,
-                }
-                task = get_task(j_task)
-                task.user_id = uid
-                task.save()
-                taskcount, id_list = send_task_prepare_sync(task)
-
-                mobile_list = Mobiles.split(',')
-                smsSendResponse = WxltTools.gen_send_rsp_xml(pinid, '0000',
-                                                             [{'id': id_list[i], 'mobile': mobile_list[i]} for i in
-                                                              range(min(len(id_list), len(mobile_list)))])
-            else:
-                smsSendResponse = WxltTools.gen_send_rsp_xml(pinid, '0001', [])
-        else:
-            smsSendResponse = WxltTools.gen_send_rsp_xml(pinid, '0002', [])
-
-        log(
-            title='WXLT_SERVICE_SMS_SEND_RESOONSE:%s' % (smsSendResponse),
-            content=None,
-            logger='spyne.application',
-            level='debug'
-        )
+        smsSendResponse = smsSend(smsSendRequest)
         return smsSendResponse
 
     @rpc(Unicode, _returns=Unicode)
     def smsGetReport(self, smsGetReportRequest):
-        log(
-            title='WXLT_SERVICE_SMS_GET_REPORT_REQUEST:%s' % (smsGetReportRequest),
-            content=None,
-            logger='spyne.application',
-            level='debug'
-        )
-        root, pinid, user_id = WxltTools.parse_req_xml_head(smsGetReportRequest)
-        smsGetReportResponse = ''
-        if user_id:
-            uid = user_id
-            sms_list = []
-            for i in range(100):
-                msg = redis_pop_obj('status_report', uid)
-                if msg is None:
-                    break
-                else:
-                    sms_list.append(msg)
-            smsGetReportResponse = WxltTools.gen_rpt_rsp_xml(pinid, '0000', sms_list)
-        else:
-            smsGetReportResponse = WxltTools.gen_send_rsp_xml(pinid, '0002', [])
-        log(
-            title='WXLT_SERVICE_SMS_GET_REPORT_RESPONSE:%s' % (smsGetReportResponse),
-            content=None,
-            logger='spyne.application',
-            level='debug'
-        )
+        smsGetReportResponse = smsGetReport(smsGetReportRequest)
         return smsGetReportResponse
 
     @rpc(Unicode, _returns=Unicode)
     def smsReceive(self, smsReceiveRequest):
-        log(
-            title='WXLT_SERVICE_SMS_RECEIVE_REQUEST:%s' % (smsReceiveRequest),
-            content=None,
-            logger='spyne.application',
-            level='debug'
-        )
-        root, pinid, user_id = WxltTools.parse_req_xml_head(smsReceiveRequest)
-        smsReceiveResponse = ''
-        if user_id:
-            uid = user_id
-            sms_list = []
-            for i in range(100):
-                msg = redis_pop_obj('recv_sms', uid)
-                if msg is None:
-                    break
-                else:
-                    sms_list.append(msg)
-            smsReceiveResponse = WxltTools.gen_recv_rsp_xml(pinid, '0000', sms_list)
-        else:
-            smsReceiveResponse = WxltTools.gen_recv_rsp_xml(pinid, '0002', [])
-        log(
-            title='WXLT_SERVICE_SMS_RECEIVE_RESPONSE:%s' % (smsReceiveResponse),
-            content=None,
-            logger='spyne.application',
-            level='debug'
-        )
+        smsReceiveResponse = smsReceive(smsReceiveRequest)
         return smsReceiveResponse
 
 
