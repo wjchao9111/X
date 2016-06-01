7229d460d8cd26da334f8473d5119f64ceedf95f
wjchao9111 <wjchao9111@sina.com>
2016-06-01 12:19:02 +0800

    修改发票管理窗口大小

--- X/static/ExtJS/view/SI_InvoiceEditWindow.js
+++ X/static/ExtJS/view/SI_InvoiceEditWindow.js
@@ -1,7 +1,7 @@
 Ext.define('X.view.SI_InvoiceEditWindow', {
     extend: 'X.util.Window',
 
-    height: 550,
+    height: 480,
     width: 400,
     base_title: '发票',
     base_url: 'extra/si-invoice-{0}/',
