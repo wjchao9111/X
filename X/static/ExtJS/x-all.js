bcfbb1675ad23485f18df48f7725f508d78cd6ca
wjchao9111 <wjchao9111@sina.com>
2016-06-14 13:08:21 +0800

    修改 String.prototype.startWith

--- X/static/ExtJS/x-all.js
+++ X/static/ExtJS/x-all.js
@@ -51,6 +51,30 @@ if (!String.prototype.getWidth) {
     }
 }
 
+if (!String.prototype.startWith) {
+    String.prototype.startWith = function (str) {
+        if (str == null || str == "" || this.length == 0 || str.length > this.length)
+            return false;
+        if (this.substr(0, str.length) == str)
+            return true;
+        else
+            return false;
+        return true;
+    }
+}
+
+if (!String.prototype.endWith) {
+    String.prototype.endWith = function (str) {
+        if (str == null || str == "" || this.length == 0 || str.length > this.length)
+            return false;
+        if (this.substring(this.length - str.length) == str)
+            return true;
+        else
+            return false;
+        return true;
+    }
+}
+
 if (!Date.prototype.format) {
     Date.prototype.format = function (fmt) { //author: meizz
         var o = {
